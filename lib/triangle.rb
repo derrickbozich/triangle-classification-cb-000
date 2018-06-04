86b09a7e6ed51293fef4866e80a5620ca8c3746f
derrick b <dbozich1@gmail.com>
2018-06-04 20:41:25 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,31 @@
 class Triangle
 
+  attr_accessor :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if @a > 0 && @a == @b && @b == @c
+      :equilateral
+    elsif side1 
+      
+  
+    end
+    
+    
+  end
+
+  def sum_side(a,b,c)
+    a+b > c ? true : false
+  end
+
+  def isosceles?
+    array = [@a,@b,@c]
+  end
   
   class TriangleError < StandardError
     def message
